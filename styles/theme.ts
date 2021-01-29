296f54dd68bda6439ab62efc5417bf25cea7ce7f
heesu <oung1002@snu.ac.kr>
2021-01-29 21:35:00 +0900

    Add style

---
+++ styles/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+  palette: {
+    primary: '#f45d48',
+    lightgray: '#e2e2e2',
+    gray: '#8a8a8a',
+  },
+  typography: {
+    title: {
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '2rem',
+      fontWeight: 600,
+    },
+    subheading: {
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '1.25rem',
+      fontWeight: 700,
+    },
+  },
+};
+
+export default theme;
