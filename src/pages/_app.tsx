95e8937cff0d3bec678e1de7b8933592eaf0e4eb
heesu <oung1002@snu.ac.kr>
2021-01-29 21:36:20 +0900

    Add basic page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
-import '../../styles/globals.css';
-
+import { ThemeProvider, Global } from '@emotion/react';
+import theme from 'styles/theme';
+import reset from 'styles/reset';
+import { Header, Footer } from '@/components/organisms';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={reset} />
+      <Header />
+      <Component {...pageProps} />;
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+
+import { Text } from '@/components/atoms';
 
 export default function Home() {
-  return <div>ㅎㅇㅎㅇ</div>;
+  return <Text typo="title"> hi </Text>;
 }
