bdd3711d32dfabec5e690dbadc51e7fde1ada4b5
heesu <oung1002@snu.ac.kr>
2021-01-30 17:48:34 +0900

    Add Home template

--- src/components/templates/Home/index.tsx
+++ src/components/templates/Home/index.tsx
@@ -1,10 +1,5 @@
-import { Text } from '@/components/atoms';
 import { BackgroundImg } from '@/components/molcules';
-
+import { PostFeed } from '@/components/organisms';
 export default function Home() {
-  return (
-    <BackgroundImg>
-      <Text typo="title"> hi </Text>
-    </BackgroundImg>
-  );
+  return <BackgroundImg>{/* <PostFeed /> */}</BackgroundImg>;
 }
