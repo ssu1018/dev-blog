f1be77b619b58e99541471a29a5131b7a204eae5
heesu <oung1002@snu.ac.kr>
2021-01-30 17:47:21 +0900

    change props

--- src/components/atoms/Text/index.tsx
+++ src/components/atoms/Text/index.tsx
@@ -1,19 +1,20 @@
 /**@jsxRuntime classic */
 /** @jsx jsx */
 
-import { jsx, typoProp, css } from '@emotion/react';
-import styled from '@emotion/styled';
+import { jsx, paletteProp, typoProp } from '@emotion/react';
 import { ReactNode } from 'react';
+
 interface LinkProps extends MarginProps {
   children: ReactNode;
   typo: typoProp;
+  color?: paletteProp;
 }
 interface MarginProps {
   mr?: string;
   mt?: string;
 }
 
-function Text({ children, typo, mr, mt }: LinkProps) {
+function Text({ children, typo, mr, mt, color }: LinkProps) {
   return (
     <p
       css={(theme) => {
@@ -21,6 +22,7 @@ function Text({ children, typo, mr, mt }: LinkProps) {
           ...theme.typography[typo],
           marginTop: mt,
           marginRight: mr,
+          color,
         };
       }}
     >
