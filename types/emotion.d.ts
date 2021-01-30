a64f25e77c81de9a2dcd1de097dff25548908083
heesu <oung1002@snu.ac.kr>
2021-01-30 17:45:49 +0900

    Fix type

--- types/emotion.d.ts
+++ types/emotion.d.ts
@@ -1,88 +1,33 @@
 import '@emotion/react';
 import theme from '../styles/theme';
 
-interface customTheme {
-  palette: {
-    primary: string;
-    lightgray: string;
-    gray: string;
-  };
-  typography: {
-    title: {
-      fontFamily: string;
-      fontSize: string;
-      fontWeight: number;
-    };
-
-    subheading: {
-      fontFamily: string;
-      fontSize: string;
-      fontWeight: number;
-    };
-  };
+interface Font {
+  fontFamily: string;
+  fontSize: string;
+  fontWeight: number;
+  color?: string;
 }
+
+type customTheme = typeof theme;
+
 // interface customTheme {
 //   palette: {
-//     primary: '#f45d48';
-//     secondary: '#0a8080';
-//     background: '#f8f5f2';
-//     white: '#ffffff';
-//     black: '#222525';
-//     lightgray: '#e2e2e2';
-//     gray: '#8a8a8a';
-//     darkgray: '#505050';
+//     primary: string;
+//     lightgray: string;
+//     gray: string;
+//     white: string;
+//     indigo: string;
 //   };
 //   typography: {
-//     title: {
-//       fontFamily: '"Gelasio", serif';
-//       fontSize: '4rem';
-//       fontWeight: 600;
-//     };
-//     subtitle: {
-//       fontFamily: '"Gelasio", serif';
-//       fontSize: '2.5rem';
-//       fontWeight: 600;
-//       lineHeight: 1.1;
-//     };
-//     heading: {
-//       fontFamily: '"Gelasio", serif';
-//       fontSize: '1.5rem';
-//       fontWeight: 500;
-//     };
-//     subheading: {
-//       fontFamily: '"Lato", sans-serif';
-//       fontSize: '1.25rem';
-//       fontWeight: 700;
-//     };
-//     label: {
-//       fontFamily: '"Lato", sans-serif';
-//       fontWeight: 700;
-//     };
-//     description: {
-//       fontFamily: '"Lato", sans-serif';
-//       lineHeight: 1.5;
-//     };
-//     body: {
-//       fontFamily: '"Lato", sans-serif';
-//     };
-//     caption: {
-//       fontFamily: '"Lato", sans-serif';
-//       fontSize: '0.875rem';
-//     };
-//     button: {
-//       fontFamily: '"Lato", sans-serif';
-//       fontSize: '0.875rem';
-//     };
-//     textbutton: {
-//       fontFamily: '"Lato", sans-serif';
-//       fontWeight: 700;
-//       fontSize: '1rem';
-//       border: 0;
-//     };
+//     title: Font;
+//     subheading: Font;
+//     whiteHeading: Font;
+//     descript: Font;
 //   };
 // }
 
 declare module '@emotion/react' {
+  // export interface Theme extends customTheme {}
   export interface Theme extends customTheme {}
 }
 
