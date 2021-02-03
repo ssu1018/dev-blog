da6ed4be2224f843222eac7e19478ae9fa97eda8
heesu <oung1002@snu.ac.kr>
2021-02-03 17:59:51 +0900

    Add typograyphy, change reset.css

---
+++ src/styles/theme.ts
@@ -0,0 +1,48 @@
+const theme = {
+  palette: {
+    primary: '#f45d48',
+    lightgray: '#e2e2e2',
+    gray: '#8a8a8a',
+    white: 'white',
+    indigo: '#748ffc',
+    black: '#000000',
+  },
+  typography: {
+    postTitle: {
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '4rem',
+      fontWeight: 600,
+    },
+    title: {
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '2rem',
+      fontWeight: 600,
+    },
+    subheading: {
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '1.25rem',
+      fontWeight: 700,
+    },
+    whiteHeading: {
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '2rem',
+      fontWeight: 600,
+    },
+    descript: {
+      fontFamily: '"Lato",sans-serif',
+      fontSize: '1rem',
+      fontWeight: 500,
+    },
+    tag: {
+      fontFamily: ' "Fira Sans", sans-serif',
+      fontSize: '1rem',
+      fontWeight: 600,
+    },
+    caption: {
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '0.875rem',
+    },
+  },
+};
+
+export default theme;
