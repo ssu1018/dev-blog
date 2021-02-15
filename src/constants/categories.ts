c26a2b251eb3c718caca0de687ace29fd25d3a4d
heesu <oung1002@snu.ac.kr>
2021-02-15 23:19:29 +0900

    Add mediaQuery,pagenation

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -6,8 +6,6 @@ export interface ICategoryProp {
   info: IPageProp;
 }
 
-export type TCategoryName = 'ALGORITHM' | 'LIFE' | 'TECH';
-
 export const categories: ICategoryProp[] = [
   {
     title: '알고리즘',
@@ -25,3 +23,11 @@ export const categories: ICategoryProp[] = [
     info: Page.TECH,
   },
 ];
+
+export enum categoryName {
+  AlGORITHM = 'ALGORITHM',
+  LIFE = 'LIFE',
+  TECH = 'TECh',
+}
+
+export type TCategoryName = keyof typeof categoryName;
